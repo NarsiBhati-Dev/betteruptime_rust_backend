79aea4d2e3311b606376831a79f3084813eff2dc
NarsiBhati-Dev <narsibhati31102@gmail.com>
2025-08-17 13:07:57 +0530

    added poem and create route

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,7 +1,21 @@
-use std::fmt::Error;
+use poem::{Route, Server, get, handler, listener::TcpListener, post, web::Path};
+
+#[handler]
+fn get_website(Path(name): Path<String>) -> String {
+    format!("hello: {}", name)
+}
+
+#[handler]
+fn create_website(Path(name): Path<String>) -> String {
+    format!("hello: {}", name)
+}
 
 #[tokio::main]
-async fn main() -> Result<(), Error> {
-    println!("Hello, world!");
-    Ok(())
+async fn main() -> Result<(), std::io::Error> {
+    let app = Route::new()
+        .at("/status/:website_id", get(get_website))
+        .at("website", post(create_website));
+    Server::new(TcpListener::bind("0.0.0.0:3001"))
+        .run(app)
+        .await
 }
